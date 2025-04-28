767405e4acc91d04a2a0e50848ee4ac1fe846965
mohammadne <mohammadne.dev@gmail.com>
2025-04-28 23:16:15 +0330

    fix: update uuid and naming convensions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-hello_world_version="v0.1.3"
 output_directory="./outputs"
 
-docker run -d --name hello_world ghcr.io/mohammadne/hello-world:$hello_world_version
-docker exec -it hello_world /app/entrypoint executer
-docker cp hello_world:/app/outputs $output_directory
-docker rm -f hello_world -t 0
+# gozar_version="v0.2.0"
+# podman run -d --name gozar ghcr.io/mohammadne/gozar:$gozar_version
+# podman exec -it gozar /app/entrypoint executer
+# podman cp gozar:/app/outputs $output_directory
+# podman rm -f gozar -t 0
 
 server_xray_directory="/root/xray"
 
 ssh proxy-server -t 'rm -rf '"$server_xray_directory"''
 scp -r "$output_directory/server" proxy-server:$server_xray_directory
-ssh proxy-server -t 'cd '"$server_xray_directory"' && docker compose up -d'
+ssh proxy-server -t 'cd '"$server_xray_directory"' && docker compose -f compose.todo.yml up -d'
 
-cd "$output_directory/client" && docker compose up -d
+# cd "$output_directory/client" && podman compose -f compose-notls.yml up -d
+podman run --restart=always -d --name "xray-notls" -p 10808:10808 -v "./notls.json:/usr/local/xray/config.json:z" "xray" run -c config.json
+
+podman run --restart=always -d --name "xray-reality" -p 10809:10809 -v "./reality.json:/usr/local/xray/config.json:z" "xray" run -c config.json
